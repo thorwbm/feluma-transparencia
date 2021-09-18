255768e0eec2bc8822532542215efe6e98cafa7d
wemerson maduro <lagoawb@gmail.com>
2021-09-17 21:52:39 -0300

    feat: criacao do projeto transparencia - feluma

---
+++ backend/src/main/java/com/feluma/transparencia/TransparenciaApplication.java
@@ -0,0 +1,13 @@
+package com.feluma.transparencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransparenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransparenciaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/feluma/transparencia/TransparenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feluma.transparencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransparenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
