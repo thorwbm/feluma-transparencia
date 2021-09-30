ca448fa6e919db4554a55dc3d74585bb61279331
wemerson maduro <lagoawb@gmail.com>
2021-09-30 00:10:47 -0300

    feat: criacao de types e do componente card

--- frontend/src/pages/Resultado/Resultado.tsx
+++ frontend/src/pages/Resultado/Resultado.tsx
@@ -1,5 +1,6 @@
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
 
 const Resultado = () => {
     return (
@@ -7,9 +8,13 @@ const Resultado = () => {
             <NavBar />
             <div className="container text-center">
                 <h1 className="text-primary py-3">Resultado</h1>
-                <div>
-
-                </div>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <Link to="/" className="btn btn-primary">Go somewhere</Link>
+                    </div>
+                 </div>
             </div>
             <Footer />
         </>
---
+++ frontend/src/types.ts
@@ -0,0 +1,9 @@
+export type RegistroDiploma = {
+    id: number;
+    aluno: string;
+    curso: string;
+    livro: string;
+    pagina: string;
+    entidadeExpedidora: string;
+    
+}
