481121c94654935759f1dea5910aaab11200fbcf
wemerson maduro <lagoawb@gmail.com>
2021-09-29 21:50:12 -0300

    feat: adicionando axios ao sistema

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'assets/css/styles.css';
 import App from 'components/App';
+import http from './utils/http'
 
 
+http.get('/alunos');
 ReactDOM.render(
   <React.StrictMode>
     <App />
