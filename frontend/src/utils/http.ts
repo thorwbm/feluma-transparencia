bae795d5e4dddecbf6295c6f36e953e7868c3467
wemerson maduro <lagoawb@gmail.com>
2021-10-15 20:13:02 -0300

    corrigindo a base_url

--- frontend/src/utils/http.ts
+++ frontend/src/utils/http.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const http = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080'
 })
 
 export default http
